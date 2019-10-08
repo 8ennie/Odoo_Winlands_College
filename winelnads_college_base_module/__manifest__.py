dcd97a8d8f2e44925f55b342134526fc17a5f2d9
tomashege <41945595+tomashege@users.noreply.github.com>
2019-10-08 20:07:28 +0200

    janky name fix

--- winelnads_college_base_module/__manifest__.py
+++ winelnads_college_base_module/__manifest__.py
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'security/wineLands_security.xml',
+        # 'security/winelands_security.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/college_menu.xml',
