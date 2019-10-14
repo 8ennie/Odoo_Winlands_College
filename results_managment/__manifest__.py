f3dc092848d20ea83d3c955b2234434966b46059
23725648 <23725648@sun.ac.za>
2019-10-14 14:16:37 +0200

    change stuff

--- results_managment/__manifest__.py
+++ results_managment/__manifest__.py
@@ -27,8 +27,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/results_menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
