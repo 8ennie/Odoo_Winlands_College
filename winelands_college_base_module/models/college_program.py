28617c737c6cfb4e80046a704305c24e05f42e55
tomashege <41945595+tomashege@users.noreply.github.com>
2019-10-19 11:24:33 +0200

    adding things

--- winelands_college_base_module/models/college_program.py
+++ winelands_college_base_module/models/college_program.py
@@ -14,8 +14,8 @@ class CollegeProgram(models.Model):
 
     #Attributes
     name = fields.Char('Name')
-    length = fields.Integer('Length')
-    # qualification = fields.Selection([('Higher Certificate',''),('National Certificate',''),('National Higher Certificate',''),('Advanced Certificate',''),('Diploma, Higher Diploma, National Diploma & Advanced Diploma',''),('Baccalaureus Technologiae (B Tech)',''),('Bachelor’s Degree',''),('Postgraduate Certificate and Diploma',''),('Bachelor',''),('Honours Degree',''),('Magister Technologiae (M Tech)',''),('Master’s Degree',''),('Doctor Technologiae (D Tech) and Doctoral Degree','')],string='Qualification')
-    qualification = fields.Selection([('',''),],string='Qualification')
+    length = fields.Integer('Length', help = 'How many years is this program')
+    qualification = fields.Selection([('Higher Certificate','Higher Certificate'),('National Certificate','National Certificate'),('National Higher Certificate','National Higher Certificate'),('Advanced Certificate','Advanced Certificate'),('Diploma, Higher Diploma, National Diploma & Advanced Diploma','Diploma, Higher Diploma, National Diploma & Advanced Diploma'),('Baccalaureus Technologiae (B Tech)','Baccalaureus Technologiae (B Tech)'),('Bachelor’s Degree','Bachelor’s Degree'),('Postgraduate Certificate and Diploma','Postgraduate Certificate and Diploma'),('Bachelor','Bachelor'),('Honours Degree','Honours Degree'),('Magister Technologiae (M Tech)','Magister Technologiae (M Tech)'),('Master’s Degree','Master’s Degree'),('Doctor Technologiae (D Tech) and Doctoral Degree','Doctor Technologiae (D Tech) and Doctoral Degree')],string='Qualification')
+    #qualification = fields.Selection([('',''),],string='Qualification')
 
 
