519e1ab629bdacc039fb8f67e80782b0f1838af1
tomashege <41945595+tomashege@users.noreply.github.com>
2019-10-19 10:56:27 +0200

    my db is not woring

--- winelands_college_base_module/models/college_program.py
+++ winelands_college_base_module/models/college_program.py
@@ -15,4 +15,7 @@ class CollegeProgram(models.Model):
     #Attributes
     name = fields.Char('Name')
     length = fields.Integer('Length')
+    # qualification = fields.Selection([('Higher Certificate',''),('National Certificate',''),('National Higher Certificate',''),('Advanced Certificate',''),('Diploma, Higher Diploma, National Diploma & Advanced Diploma',''),('Baccalaureus Technologiae (B Tech)',''),('Bachelor’s Degree',''),('Postgraduate Certificate and Diploma',''),('Bachelor',''),('Honours Degree',''),('Magister Technologiae (M Tech)',''),('Master’s Degree',''),('Doctor Technologiae (D Tech) and Doctoral Degree','')],string='Qualification')
     qualification = fields.Selection([('',''),],string='Qualification')
+
+
