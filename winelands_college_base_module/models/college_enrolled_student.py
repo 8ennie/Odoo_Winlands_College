9a009fc19ae9bc8cee7c10d2f231ead6487baec0
tomashege <41945595+tomashege@users.noreply.github.com>
2019-10-21 22:23:00 +0200

    test

--- winelands_college_base_module/models/college_enrolled_student.py
+++ winelands_college_base_module/models/college_enrolled_student.py
@@ -2,6 +2,7 @@
 from odoo.exceptions import ValidationError
 from odoo import models, fields, api
 import datetime
+from fpdf import FPDF
 
 class CollegeEnrolledStudent(models.Model):
     _name = 'college.enrolledstudent'
@@ -15,6 +16,7 @@ class CollegeEnrolledStudent(models.Model):
     year = fields.Char('Year', size=4,default= str(datetime.datetime.now().year))
     mark = fields.Float('Mark')
 
+    
     @api.constrains('mark')
     def _mark_validation(self):
         for value in self:
@@ -23,4 +25,20 @@ class CollegeEnrolledStudent(models.Model):
             if value.mark<0:
                 raise ValidationError("The mark may not be negitive")
 
+    # def create_pdf(list_of_results,id_wcw):
+    #     pdf = FPDF(orientation='P', unit='mm', format='A4')
+    #     pdf.add_page()
+    #     pdf.set_font("Arial", size=30)
+    #     pdf.set_text_color(255,0,0)  
+    #     pdf.cell(2, 10, txt= "Not for official purposes", ln=100, align = "l")
+    #     heading_text =  "wine lands collage academic record for student: " + str(id_wcw)
+    #     pdf.set_font("Arial", size=20)
+    #     pdf.set_text_color(0,0,0) 
+    #     pdf.cell(2, 20, txt=heading_text , ln=100, align = "l")
+    #     pdf.set_font("Arial", size=20)
+    #     pdf.set_text_color(0,0,0) 
+    #     for mark in list_of_results:
+    #         pdf.cell(2, 10, txt= mark, ln=100, align = "l")
+    #     pdf.output("Academic_Transcript_"+str(id_wcw)+".pdf")
+    # create_pdf(['hey','hey'], 23)
 
