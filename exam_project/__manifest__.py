68d7ed8f2e04845ecec50b653627b8887d4d4414
tomashege <41945595+tomashege@users.noreply.github.com>
2019-11-03 14:23:33 +0200

    edit

---
+++ exam_project/__manifest__.py
@@ -0,0 +1,14 @@
+{
+ 'name': 'todo',
+ 'description': 'This is an App that will allow use to store a todo list',
+ 'author': 'Tomas Hegewisch',
+ 'depends': ['base'],
+ 'application': True,
+ 'data': [
+
+ ],
+ 'demo': [
+
+],
+
+}
