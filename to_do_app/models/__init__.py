2d6de455b4fc23c9baf96ef20f13a610b55f6f12
tomashege <41945595+tomashege@users.noreply.github.com>
2019-11-04 16:27:26 +0200

    new vlaues

--- to_do_app/models/__init__.py
+++ to_do_app/models/__init__.py
@@ -1,3 +1,4 @@
 from . import todo_list
 from . import person
 from . import dependent
+from . import link
---
+++ to_do_app/models/product.py
@@ -0,0 +1,7 @@
+from odoo import fields, models, api
+import random
+class link(models.Model):
+    _name = 'todo.product'
+    _description = 'this is to make a link'
+    
+
